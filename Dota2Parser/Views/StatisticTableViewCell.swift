571c1ada6fbe29c67a287352488052ba6f00395e
Fedor Eronin <eroninf@icloud.com>
2022-07-15 21:49:55 +0300

    add  StatisticTableViewCell

---
+++ Dota2Parser/Views/StatisticTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StatisticTableViewCell.swift
+//  Dota2Parser
+//
+//  Created by Федор Еронин on 15.07.2022.
+//
+
+import UIKit
+
+class StatisticTableViewCell: UITableViewCell {
+    
+    @IBOutlet var heroImage: UIImageView!
+    @IBOutlet var heroName: UILabel!
+    @IBOutlet var winsLabel: UILabel!
+    @IBOutlet var losesLabel: UILabel!
+    @IBOutlet var winrateLabel: UILabel!
+}
