ca105a54bbb92e6560ddf5704b09fa453e7d8137
Fedor Eronin <eroninf@icloud.com>
2022-07-15 22:36:35 +0300

    remove setTitle method to TabBarViewController

--- Dota2Parser/Controllers/PopularItemsTableViewController.swift
+++ Dota2Parser/Controllers/PopularItemsTableViewController.swift
@@ -34,7 +34,6 @@ class PopularItemsTableViewController: UITableViewController {
         fetchData()
         setBackground()
         setActivityIndicator()
-        setTitle()
     }
     
     
@@ -78,38 +77,7 @@ class PopularItemsTableViewController: UITableViewController {
     }
     
     
-    private func setTitle() {
-        
-        let titlelabel = UILabel()
-        titlelabel.text = "  " + heroName
-        titlelabel.font = UIFont.boldSystemFont(ofSize: 16)
-        titlelabel.sizeToFit()
-        titlelabel.textAlignment = .center
-        
-        let titleView = UIView()
-        titlelabel.center = titleView.center
-        
-        guard let url = URL(string: "http://cdn.dota2.com" + heroIcon)
-        else { return }
-        let heroIconPic = UIImageView()
-        heroIconPic.kf.setImage(with: url, placeholder: UIImage(named: "heroIconPlaceholder"))
-        guard let _ = heroIconPic.image else { return }
-        let heroIconAspect = heroIconPic.image!.size.width /
-        heroIconPic.image!.size.height
-        heroIconPic.frame = CGRect(
-            x: titlelabel.frame.origin.x -
-            titlelabel.frame.size.height * heroIconAspect,
-            y: titlelabel.frame.origin.y,
-            width: titlelabel.frame.size.height * heroIconAspect,
-            height: titlelabel.frame.size.height
-        )
-        heroIconPic.contentMode = .scaleAspectFill
-        
-        titleView.addSubview(titlelabel)
-        titleView.addSubview(heroIconPic)
-        
-        navigationItem.titleView = titleView
-    }
+
     
     
     // MARK: - Navigation
